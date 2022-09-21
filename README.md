85100eb7e25fda7f22e0606b3371e6a70cab33c0
OceanPresent <oceanpresent@163.com>
2022-09-21 23:47:52 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,33 @@
+# starter-tauri
+
+This template should help get you started developing with Tauri and Vue3 with unplugin in Vite.
+
+
+## Feature
+- Unocss
+- Unplugin supports: vue-use、vue3
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.vscode-typescript-vue-plugin).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+pnpm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm tauri dev 
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm tauri build
---
+++ vite.config.ts
@@ -0,0 +1,50 @@
+import * as path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import Unocss from 'unocss/vite'
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    vue(),
+    Components({
+      // 指定组件位置，默认是src/components
+      dirs: ['src/components'],
+      extensions: ['vue'],
+      // 配置文件生成位置
+      dts: 'src/components.d.ts',
+    }),
+    AutoImport({
+      imports: ['vue', '@vueuse/head', '@vueuse/core'],
+      // 可以选择auto-import.d.ts生成的位置，使用ts建议设置为'src/auto-import.d.ts'
+      dts: 'src/auto-import.d.ts',
+    }),
+    Unocss(),
+  ],
+
+  // Vite optons tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+  // tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+  },
+  // to make use of `TAURI_DEBUG` and other env variables
+  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    // Tauri supports es2021
+    target: ['es2021', 'chrome100', 'safari13'],
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
+})
